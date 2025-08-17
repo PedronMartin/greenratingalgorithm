89e5bc2ae143c22ff50da8a66107f69e42dfe21e
Martin <martingiuseppe.pedron@studenti.univr.it>
2025-08-17 17:10:42 +0200

    Versione v3 con input e output migliorati

--- Algorithm/algorithm.py
+++ Algorithm/algorithm.py
@@ -60,4 +60,44 @@ class GreenRatingAlgorithm(QgsProcessingAlgorithm):
         return GreenRatingAlgorithm()
 
     def processAlgorithm(self, parameters, context, feedback):
-        return {}
+        # Recupera gli strati di input dall'interfaccia utente
+        edifici_input = self.parameterAsSource(
+            parameters,
+            self.INPUT_BUILDINGS,
+            context
+        )
+
+        feedback.pushInfo("Verifica dei layer di input...")
+
+        if edifici_input is None:
+            feedback.reportError("Errore: Impossibile caricare lo strato degli edifici.")
+            return {}
+        
+        feedback.pushInfo("Layer di input caricato con successo.")
+
+        # Inizializza il layer di output
+        (sink, dest_id) = self.parameterAsSink(
+            parameters,
+            self.OUTPUT,
+            context,
+            edifici_input.fields(),
+            edifici_input.wkbType(),
+            edifici_input.sourceCrs()
+        )
+
+        # Inizializza il contatore di progresso
+        total_features = edifici_input.featureCount()
+
+        # Itera su ogni elemento (edificio) del layer di input e lo scrive nell'output
+        for i, feature in enumerate(edifici_input.getFeatures()):
+            if feedback.isCanceled():
+                break
+            
+            # Aggiorna la barra di progresso
+            feedback.setProgress(int((i / total_features) * 100))
+            
+            # Scrivi la feature nell'output
+            sink.addFeature(feature)
+
+        # Restituisce l'output
+        return {self.OUTPUT: dest_id}
